d6b0ee453a5a050f5e741dae76bf47e67fb4e9b4
lukas <lukasrocha0402@gmail.com>
2024-03-21 12:17:34 -0300

    Adicionando um algoritimo sobre operadores logicos

---
+++ ola_mundo/src/main/java/org/example/Operadores_logicos_relacionais.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Operadores_logicos_relacionais {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 2;
+        b = 3;
+        c = 5;
+
+        if (a * b <= c)
+            System.out.println("A é maior ou igual");
+        else
+            System.out.println("A é menor ");
+    }
+}
