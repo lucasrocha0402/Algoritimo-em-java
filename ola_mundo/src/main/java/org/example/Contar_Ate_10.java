0385157332e5815ae97d218140c79e4daa44ed0d
lukas <lukasrocha0402@gmail.com>
2024-03-22 19:10:51 -0300

    Adicionando algoritimo de contador até 10

---
+++ ola_mundo/src/main/java/org/example/Contar_Ate_10.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Contar_Ate_10 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int o = 0;
+        do {
+            System.out.println(o);
+            o++;
+        } while (o <= 10);
+    }
+}
