a5db8ab4d06f4f3090e932b39b1ccc73bb2832eb
lukas <lukasrocha0402@gmail.com>
2024-03-25 22:40:19 -0300

    Adicionando um algorito que realiza a combinação de variaveis

---
+++ ola_mundo/src/main/java/org/example/Combinacao.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Combinacao {
+    public static void main(String[] args) {
+        int c1, c2;
+        for (c1 = 1; c1 <= 3; c1 ++){
+            for (c2 = 1; c2 <= 3; c2 ++)
+        System.out.println(c1 + " " + " " + c2);
+        }
+    }
+}
