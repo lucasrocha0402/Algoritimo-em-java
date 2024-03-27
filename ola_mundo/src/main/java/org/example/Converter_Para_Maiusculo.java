1d5d19ecbc5309460c0d9aa176b1865bc9311d27
lukas <lukasrocha0402@gmail.com>
2024-03-26 22:03:34 -0300

    Adicionando o algoritimo que converte a string para minusculo

---
+++ ola_mundo/src/main/java/org/example/Converter_Para_Maiusculo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Converter_Para_Maiusculo {
+
+    public static String ConverterParaMaiusculo(String nome){
+        return nome.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o nome que deseja converter para maiusculo: ");
+        String nome = teclado.nextLine();
+        String nomeMaiusculo = ConverterParaMaiusculo(nome);
+        System.out.println("Nome em maiusculo: " + nomeMaiusculo);
+    }
+}
