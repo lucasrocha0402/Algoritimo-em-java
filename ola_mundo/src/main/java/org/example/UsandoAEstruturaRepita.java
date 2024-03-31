d3c716760e0fbdf4006677324494ae5819a809cf
lukas <lukasrocha0402@gmail.com>
2024-03-31 11:36:44 -0300

    Adicionando algoritimo que usa estrutura de repetição

---
+++ ola_mundo/src/main/java/org/example/UsandoAEstruturaRepita.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UsandoAEstruturaRepita {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int c = 1;
+        for (; c <= 10; c ++){
+         System.out.println(c);
+         }
+    }
+}
